94e80babe366d30a326bb7c78af800e138387e98
Youngjae so <terranbang@gmail.com>
2026-05-25 19:40:40 +0900

    feat : picngo 백엔드 프로젝트 초기 설정

---
+++ src/main/java/com/project/picngo/PicngoApplication.java
@@ -0,0 +1,13 @@
+package com.project.picngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicngoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicngoApplication.class, args);
+	}
+
+}
